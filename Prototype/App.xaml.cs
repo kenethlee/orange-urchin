8b87fdb912fb4d0dd893bba875791e679d409cad
Kenneth Lee <kenneth.lee@7dsurgical.com>
2019-10-20 04:26:10 -0400

    find existing unanswered question

--- Prototype/App.xaml.cs
+++ Prototype/App.xaml.cs
@@ -15,11 +15,9 @@ namespace Prototype
             var viewModel = new MainWindowViewModel(kb);
             MainWindow = new MainWindow {DataContext = viewModel };
             MainWindow.Show();
-
-            viewModel.Update();
         }
 
-        private string _token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdJZCI6IjE4MGRiYTk1LTFhYjYtNDRiMC05Yzk0LTQ2MzBlOGQyODBiZiIsImV4cCI6MTU3MTU0NzMyOSwiaWF0IjoxNTcxNTQzNzI5fQ.7SgahurxtRh3V3DESCJfU-Akn0HJPPpN38Fjml1DPSI";
+        private string _token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdJZCI6IjE4MGRiYTk1LTFhYjYtNDRiMC05Yzk0LTQ2MzBlOGQyODBiZiIsImV4cCI6MTU3MTU2MjI0NSwiaWF0IjoxNTcxNTU4NjQ1fQ.2uYGLq5bSVvCbeF4S2fR-C_AJghV-LWxNQ6hq--Vbw0";
         private string _knowledgeBase = "df599a54-91bd-44c5-9a93-19d055b6ca57";
     }
 }
